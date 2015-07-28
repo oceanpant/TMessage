fff7dfc3ff31d0ac81e646bff71a7e4d09a30037
tarena <tarena@tarenadeiMac.local>
2015-07-28 09:44:18 +0800

    Initial Commit

---
+++ TMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TMessage
+//
+//  Created by tarena on 15/7/28.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
